a6c821884a2deb21fa86c81466c7e7dcdb9eac27
Matt Stubbz <mlstubblefield@speedwaymotors.com>
2022-09-15 09:16:43 -0500

    more react

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,19 @@
 import { GetServerSideProps } from "next";
+import { useEffect, useState } from "react";
 
-export default function Home() {
-  return <h1>sdfsdf</h1>;
+export default function Home({hi}) {
+  const [st, setSt] = useState()
+
+  useEffect(() => {
+    setSt(" once was a man named banana")
+  }, [])
+
+  return <h1>{hi}{st}</h1>;
 }
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   // throw new Error("boop");
   return {
-     props:{}
+     props:{ hi: "there" }
   }
 };
