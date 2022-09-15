0cbcad9012ae8a2238f94ea2c38fd4b589e6b11a
Matt Stubbz <mlstubblefield@speedwaymotors.com>
2022-09-15 10:19:01 -0500

    made the reproduction better

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import("next").NextConfig} */
 const config = {
   productionBrowserSourceMaps: true,
+  async rewrites(){
+    return [
+      {
+        source: "/",
+        destination: "/1",
+      }
+    ]
+  }
 };
 
 module.exports = config;
---
+++ pages/[...something].tsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+export default function Home({hi, something}) {
+  
+  return <div>
+    <h1>hi {hi} {something}</h1>
+    <div>
+      <Link href={`/${parseInt(something??0)+1}`} passHref><a>Next!</a></Link>
+    </div>
+  </div>;
+}
+
+export const getStaticPaths = async () => {
+  return { paths: [], fallback: 'blocking' }
+}
+
+export const getStaticProps = async (context) => {
+  // throw new Error("boop");
+  console.log(context.params)
+  return {
+     props:{ hi: "there", something: context.params.something }
+  }
+};
