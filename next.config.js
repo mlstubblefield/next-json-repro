511ced9fa01348949a5b56fc9483a827868ac101
Matt Stubbz <mlstubblefield@speedwaymotors.com>
2022-09-15 09:31:07 -0500

    some tweaks to hopefully repro

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import("next").NextConfig} */
 const config = {
   productionBrowserSourceMaps: true,
-}
+  assetPrefix: '/whatever',
+  async rewrites() {
+    return [
+      {
+        source: "/something",
+        destination: "/",
+      },
+    ];
+  },
+};
 
-module.exports = config
+module.exports = config;
