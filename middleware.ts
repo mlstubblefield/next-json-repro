2565b1431e06508c911b9d55fb283cde35c67dbc
Matt Stubbz <mlstubblefield@speedwaymotors.com>
2022-09-15 10:49:03 -0500

    forced WSOD

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,8 @@
+import { url } from "inspector";
 import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(_request: NextRequest) {
+    if(_request.url.toString().includes(".json")){
+        return NextResponse.rewrite(new URL("/404landia", _request.url))
+    }
 }
