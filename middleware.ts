fd75748d166524b0257a8a01c69392f8d384c2d4
Matt Stubbz <mlstubblefield@speedwaymotors.com>
2022-10-13 14:45:00 -0500

    updated that there canary and added set cookie

--- middleware.ts
+++ middleware.ts
@@ -2,4 +2,7 @@ import { url } from "inspector";
 import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(_request: NextRequest) {
+    const response = NextResponse.next()
+    response.cookies.set('hello', _request.url.toString())
+    return response
 }
