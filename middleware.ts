92c0619f1ae2c694276c6da825c98047ff69f4f8
Matt Stubbz <mlstubblefield@speedwaymotors.com>
2022-11-09 16:37:10 -0600

    trying to repro jon's issue

--- middleware.ts
+++ middleware.ts
@@ -5,5 +5,12 @@ export function middleware(_request: NextRequest) {
     const response = NextResponse.next()
     response.cookies.set('hello', _request.url.toString())
     response.cookies.set('hello', "new" + _request.url.toString())
-    return response
+    const url = _request.nextUrl.clone()
+    if(url.pathname == "/"){
+        url.pathname = "/somethingelse"
+        return NextResponse.rewrite(url)
+    }else{
+        return response
+    }
+    
 }
